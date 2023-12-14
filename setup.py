59672ad960194f100ccbe5602a6590a5a2de8ea5
Ebram Shehata <ebram96@gmail.com>
2023-12-14 21:00:43 +0200

    Exclude tests directory from being packaged

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cloudconvert/cloudconvert-python",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=["tests"]),
     install_requires=[
         "requests",
         "urllib3"
